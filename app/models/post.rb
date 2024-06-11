ced7fd500a2a1e082c0f88c45168428f6a043adf
Gaetano Osur <165330888+osur-gaetano@users.noreply.github.com>
2024-06-11 13:59:22 +0300

    Add reference between user and post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,11 @@
 class Post < ApplicationRecord
   belongs_to :user
 
+  validates :title, presence: true, length: {minimum:8,
+              too_short: "%{count} characters is too short for a title of your post"}
+  validates :body, presence: true, length: {
+    minimum: 4, too_short: "No way that short statement is going to your wall !"
+  }
+
 
 end
